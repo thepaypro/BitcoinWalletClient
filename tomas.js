c0414ac60d0f14db53110386e6526b3d39c6b2fe
Pau Garcia <pau.g.pujol@gmail.com>
2017-09-22 09:48:07 +0200

    project setup

---
+++ tomas.js
@@ -0,0 +1,48 @@
+var Client = require('bitcore-wallet-client');
+
+
+var fs = require('fs');
+var BWS_INSTANCE_URL = 'https://bws.bitpay.com/bws/api'
+
+var secret = process.argv[2];
+if (!secret) {
+  console.log('./tomas.js <Secret>')
+
+  process.exit(0);
+}
+
+var client = new Client({
+  baseUrl: BWS_INSTANCE_URL,
+  verbose: false,
+});
+
+client.joinWallet(secret, "Tomas", {}, function(err, wallet) {
+  if (err) {
+    console.log('error: ', err);
+    return
+  };
+
+  console.log('Joined ' + wallet.name + '!');
+  fs.writeFileSync('tomas.dat', client.export());
+
+
+  client.openWallet(function(err, ret) {
+    if (err) {
+      console.log('error: ', err);
+      return
+    };
+    console.log('\n\n** Wallet Info', ret); //TODO
+
+    console.log('\n\nCreating first address:', ret); //TODO
+    if (ret.wallet.status == 'complete') {
+      client.createAddress({}, function(err,addr){
+        if (err) {
+          console.log('error: ', err);
+          return;
+        };
+
+        console.log('\nReturn:', addr)
+      });
+    }
+  });
+});
